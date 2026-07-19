f4449f4e54545f5b9a29196762acc16945a9956a
Lamaproinfinite <nguyentunglambuncha@gmail.com>
2026-07-19 11:47:36 +0700

    initialize frontend repo

---
+++ app/page.tsx
@@ -0,0 +1,108 @@
+'use client'
+
+import { useState } from "react"
+
+
+export function TextInputAndOutput(){
+  const [prompt, setPrompt] = useState('')
+  const [llmResponse, setLlmResponse] = useState('')
+
+  async function communicateWithBackend() {
+    const payload = {'user_question' : prompt}
+    
+    //fetch code
+    const response = await fetch('http://127.0.0.1:8000/prompt', {
+      method: "POST",
+      headers: {'Content-type' : 'application/json'}, //mandatory metadata to get Pydantic to read the JSON properly
+      body: JSON.stringify(payload) //turn the variable into a string and transmit safely as JSON
+    });
+
+    const reply = await response.json();
+    setLlmResponse(reply.answer)
+    //this thing unpack the JSON the backen returns
+    }
+
+
+  //html UI display on page
+  return(
+    <div>
+      <input
+      //this is the tag that creates a typing field
+        type = 'text'
+        placeholder = 'Ask the AI something bout the context'
+        //the faded text in the box that dissapear when user type sumthin
+        value = {prompt}
+        //the displayed value
+        onChange={(typedCharacter) => setPrompt(typedCharacter.target.value)}
+        //what inside () is the recorded input, we name the variable typedCharacter
+        //So the typed character is stored in the variable typedCharacter
+        //typedCharacter.target is the input box itself
+        //.value is the value of the input box
+
+        />
+      <button onClick={() => communicateWithBackend()}>
+        Send prompt to AI
+      </button>
+
+      <p>
+        AI answer: {llmResponse}
+      </p>
+
+    </div>
+  );
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default function HomePage()
+{
+  const [numberTracker, numberSetter] = useState(0)
+
+  return (
+    <div>
+      <hr/>
+      <h1>
+        AI chat with ur source
+      </h1>
+      <TextInputAndOutput />
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
